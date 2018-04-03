e30c40cbe0d9f6ab1c78f7363f4cf551017bf6ee
João Rangel <joaohcrangel@gmail.com>
2018-04-03 15:23:24 -0300

    Aula 12

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var user = {};
 
 function addLine(dataUser){
 
-    var tr = document.createElement("tr");
+    console.log(dataUser);
 
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -18,8 +18,6 @@ function addLine(dataUser){
             </td>
         </tr>
     `;
-
-    document.getElementById("table-users").appendChild(tr);
 
 }
 
@@ -43,6 +41,17 @@ document.getElementById("form-user-create").addEventListener("submit", function(
 
     });
 
-    addLine(user);
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth,
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
+
+    addLine(objectUser);
 
 });
