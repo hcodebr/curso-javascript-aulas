b44076ba075feab62b1bb31fcefecb6bea83037d
João Rangel <joaohcrangel@gmail.com>
2018-04-02 23:48:12 -0300

    Aula 005

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var name = document.querySelector("#exampleInputName");
-
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
 
 
