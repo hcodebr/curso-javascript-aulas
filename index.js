5df87ccfc11d508ea6f28955437bfc7278e942a5
João Rangel <joaohcrangel@gmail.com>
2018-04-03 14:48:56 -0300

    Aula 011

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
-fields.forEach(function(field, index){
+function addLine(dataUser){
 
-    if (field.name == "gender") {
+    var tr = document.createElement("tr");
 
-        if (field.checked) {
-            console.log("SIM", field);
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+
+}
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+
+        } else {
+
+            user[field.name] = field.value;
+
         }
 
-    } else {
+    });
 
-        console.log("NÃO");
-
-    }
-
-    //console.log(field.id, field.name, field.value, field.checked, index);
+    addLine(user);
 
 });
