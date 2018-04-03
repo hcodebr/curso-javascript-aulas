89f48a3d28e21ba658089424d24e7e22ae385734
João Rangel <joaohcrangel@gmail.com>
2018-04-03 00:18:49 -0300

    007

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.value, field.checked, index);
+    if (field.name == "gender") {
+
+        if (field.checked) {
+            console.log("SIM", field);
+        }
+
+    } else {
+
+        console.log("NÃO");
+
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index);
 
 });
